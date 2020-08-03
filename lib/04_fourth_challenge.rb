f4f18b872a8696f5cd12d8b753abda651256f7a2
Alex Mata <alexmata92@ymail.com>
2020-08-03 18:27:01 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -4,8 +4,9 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        #your hashes here!
+      :hero_friends => [ 
+        {name: , age: , attitude: },
+        {name: , age: , attitude: }
       ]
    }, 
    :capulet => {
@@ -13,7 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {name: , age: , attitude: },
+        {name: , age: , attitude: }
       ]
    }
 }
