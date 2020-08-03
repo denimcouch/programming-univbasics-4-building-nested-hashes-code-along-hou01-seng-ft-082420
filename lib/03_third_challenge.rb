e7456c6a2741ad192dd81ddf117e8b8c86d88661
Alex Mata <alexmata92@ymail.com>
2020-08-03 18:23:11 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,13 +2,17 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => ,
+        :age => 
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => ,
+        :age =>
         },
       :hero => {
-        #your key/value pairs here
+        :name => ,
+        :age => ,
+        :status =>
         },
       :hero_friends => []
    }, 
